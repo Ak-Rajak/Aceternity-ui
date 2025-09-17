78f4eac9f41e4d1b4aff03b7772a670f9fe47ef1
Ak-Rajak <atulkumar.coderguys@gmail.com>
2025-09-18 03:30:37 +0530

    Added the featured folder

--- src/components/FeatureCourses.tsx
+++ src/components/FeatureCourses.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import courseData from "@/data/music_courses.json";
-import Link from "next/link";
+// import Link from "next/link";
 import { BackgroundGradient } from "./ui/background-gradient";
 
 interface Course {
